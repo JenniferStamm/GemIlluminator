31f38dc8d093e50ef97c9ec05437ad1f080a4fb6
Jennifer Stamm <jenniferstamm1992@gmail.com>
2014-11-30 14:27:06 +0100

    Generate random position

--- gemIlluminator/gem.cpp
+++ gemIlluminator/gem.cpp
@@ -2,9 +2,19 @@
 
 #include "gemrenderer.h"
 
+#include <ctime>
+#include <cstdlib>
+
 Gem::Gem(QObject *parent) :
     AbstractGeometry(parent)
 {
+    srand(static_cast <unsigned> (time(0)));
+    float x = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) * 2.f - 1.f;
+    float y = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) * 2.f - 1.f;
+    float z = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) * 2.f - 1.f;
+    m_position.setX(x);
+    m_position.setY(y);
+    m_position.setZ(z);
 }
 
 Gem::~Gem()
@@ -17,6 +27,8 @@ void Gem::synchronize()
     if (!m_renderer){
         m_renderer = new GemRenderer();
     }
+
+    m_renderer->setPosition(m_position);
 }
 
 void Gem::cleanup()
