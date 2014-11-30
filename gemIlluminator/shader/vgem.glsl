c7e659b7d790d8b9ba60e35b49c9b2d451d50d48
Jennifer Stamm <jenniferstamm1992@gmail.com>
2014-11-30 13:53:31 +0100

    Add shading

--- gemIlluminator/shader/vgem.glsl
+++ gemIlluminator/shader/vgem.glsl
@@ -11,9 +11,26 @@ attribute highp vec3 a_normal;
 
 varying vec3 v_color;
 
+const vec3 lightPos = vec3(0.5, 0.8, 1.0);
+const vec3 specColor = vec3(1.0, 1.0, 1.0);
+
 void main()
 {
     gl_Position = modelViewProjection * vec4(a_vertex, 1.0);
-    v_color = a_normal;
-    v_color = a_color;
+
+    vec4 vertPos4 = modelViewProjection * vec4(a_vertex, 1.0);
+    vec3 vertPos = vec3(vertPos4) / vertPos4.w;
+    vec3 lightDir = normalize(lightPos - vertPos);
+    vec3 reflectDir = reflect(-lightDir, a_normal);
+    vec3 viewDir = normalize(-vertPos);
+
+    float lambertian = max(dot(lightDir, a_normal), 0.0);
+    float specular = 0.0;
+
+    if(lambertian > 0.0) {
+        float specAngle = max(dot(reflectDir, viewDir), 0.0);
+        specular = pow(specAngle, 4.0);
+    }
+
+    v_color = lambertian * a_color + specular * specColor;
 }
