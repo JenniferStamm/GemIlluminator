a99dc2139ef6b9313dac8ae3669473efa99ad524
Jennifer Stamm <jenniferstamm1992@gmail.com>
2014-12-16 15:19:59 +0100

    Update code in presentation

--- presentations/intermediate-presentation/presentation/code/active.js
+++ presentations/intermediate-presentation/presentation/code/active.js
@@ -3,42 +3,44 @@ Connections {
 
 	onStateChanged: {
 		switch (Qt.application.state) {
-		case Qt.ApplicationSuspended:
-			scene.active = false
-			console.log("Suspended")
-			break
-		case Qt.ApplicationHidden:
-			scene.active = false
-			console.log("Hidden")
-			break
 		case Qt.ApplicationActive:
 			if(Qt.platform.os == "android") {
 				root.showFullScreen()
-
 				var types = QmlSensors.sensorTypes();
 
-				if (types.indexOf("QRotationSensor") !== -1 && Qt.platform.os == "android") {
+				if (types.indexOf("QRotationSensor") !== -1) {
 					sensorInputs.rotationSensorId.active = true
-				}
-				else if (types.indexOf("QTiltSensor") !== -1 && Qt.platform.os == "android") {
+				} else if (types.indexOf("QTiltSensor") !== -1) {
 					sensorInputs.tiltSensorId.active = true
-				}	
-				else {
-						mouseInput.enabled = true
-					}
-				}
-			scene.active = true
+				}  
+			}
+			else {
+				mouseInput.enabled = true
+			}
+
+			if(scene !== null) 
+				scene.active = true
 			console.log("Active")
 			break
 		case Qt.ApplicationInactive:
-			scene.active = false
+			if(scene !== null) 
+				scene.active = false
 
-			if(Qt.platform.os == "android") {
+			if(Qt.platform.os === "android") 
 				root.hide()
-			}
 
 			console.log("Inactive")
 			break
+		case Qt.ApplicationSuspended:
+			if(scene !== null) 
+				scene.active = false
+			console.log("Suspended")
+			break
+		case Qt.ApplicationHidden:
+			if(scene !== null) 
+				scene.active = false
+			console.log("Hidden")
+			break
 		}
-   }
+	}
 }
