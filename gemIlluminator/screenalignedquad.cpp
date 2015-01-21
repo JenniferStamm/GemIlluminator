a5de6f01cabfb3f8037e1345155854cbc505c8b6
Pascal Lange <pascal.lange@student.hpi.uni-potsdam.de>
2015-01-21 16:17:11 +0100

    Refactor ScreenAlignedQuad because OpenGL ES 2.0 don't support VertexArrayObjects (only as extension)

--- gemIlluminator/screenalignedquad.cpp
+++ gemIlluminator/screenalignedquad.cpp
@@ -5,30 +5,24 @@
 #include <QOpenGLFunctions>
 
 
-ScreenAlignedQuad::ScreenAlignedQuad(
-    QOpenGLFunctions &gl
-,	const GLuint /*vertexAttribLocation*/)
-: m_vertices(QOpenGLBuffer::VertexBuffer)
+ScreenAlignedQuad::ScreenAlignedQuad() :
+    m_vertices(QOpenGLBuffer::VertexBuffer)
 {
-	m_vao.create();
-	m_vao.bind();
-
 	m_vertices.create();
 	m_vertices.setUsagePattern(QOpenGLBuffer::StaticDraw);
+}
 
-	strip(m_vertices);
-
-	gl.glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 0, nullptr);
-	gl.glEnableVertexAttribArray(0);
-
-	m_vao.release();
+ScreenAlignedQuad::~ScreenAlignedQuad()
+{
+    m_vertices.destroy();
+    delete m_vertices;
 }
 
 void ScreenAlignedQuad::draw(QOpenGLFunctions &gl)
 {
-	m_vao.bind();
+    gl.glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 0, nullptr);
+    gl.glEnableVertexAttribArray(0);
 	gl.glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-	m_vao.release();
 }
 
 void ScreenAlignedQuad::strip(QOpenGLBuffer &vertices)
