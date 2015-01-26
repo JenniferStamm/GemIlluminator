df0b89fbe6dbb706a36d082716ca0b7861568fe4
Pascal Lange <pascal.lange@student.hpi.uni-potsdam.de>
2015-01-26 17:07:30 +0100

    Create SceneBounds in space [-1;1]^3 and use scale for size in order to support planned feature of SceneBounds configured by config.json

--- gemIlluminator/scenebounds.cpp
+++ gemIlluminator/scenebounds.cpp
@@ -8,75 +8,75 @@
 SceneBounds::SceneBounds(QObject *parent) :
     AbstractGem(parent)
 {
-    float value = 25.f;
-    m_radius = sqrt(value * value * 3.f);
-    m_scale = 1.f;
+    m_radius = sqrt(1.0f * 1.0f * 3.f);
+    m_scale = 25.f;
+    calculateModelMatrix();
 
     //front
     m_triangles->append(new Triangle(
-                            QVector3D(-value,  value,  value),
-                            QVector3D( value,  value,  value),
-                            QVector3D( value, -value,  value),
+                            QVector3D(-1.0f,  1.0f,  1.0f),
+                            QVector3D( 1.0f,  1.0f,  1.0f),
+                            QVector3D( 1.0f, -1.0f,  1.0f),
                             this));
     m_triangles->append(new Triangle(
-                            QVector3D(-value,  value,  value),
-                            QVector3D( value, -value,  value),
-                            QVector3D(-value, -value,  value),
+                            QVector3D(-1.0f,  1.0f,  1.0f),
+                            QVector3D( 1.0f, -1.0f,  1.0f),
+                            QVector3D(-1.0f, -1.0f,  1.0f),
                             this));
     //back
     m_triangles->append(new Triangle(
-                            QVector3D(-value,  value, -value),
-                            QVector3D(-value, -value, -value),
-                            QVector3D( value, -value, -value),
+                            QVector3D(-1.0f,  1.0f, -1.0f),
+                            QVector3D(-1.0f, -1.0f, -1.0f),
+                            QVector3D( 1.0f, -1.0f, -1.0f),
                             this));
     m_triangles->append(new Triangle(
-                            QVector3D(-value,  value, -value),
-                            QVector3D( value, -value, -value),
-                            QVector3D( value,  value, -value),
+                            QVector3D(-1.0f,  1.0f, -1.0f),
+                            QVector3D( 1.0f, -1.0f, -1.0f),
+                            QVector3D( 1.0f,  1.0f, -1.0f),
                             this));
     //left
     m_triangles->append(new Triangle(
-                            QVector3D(-value,  value, -value),
-                            QVector3D(-value,  value,  value),
-                            QVector3D(-value, -value,  value),
+                            QVector3D(-1.0f,  1.0f, -1.0f),
+                            QVector3D(-1.0f,  1.0f,  1.0f),
+                            QVector3D(-1.0f, -1.0f,  1.0f),
                             this));
     m_triangles->append(new Triangle(
-                            QVector3D(-value,  value, -value),
-                            QVector3D(-value, -value,  value),
-                            QVector3D(-value, -value, -value),
+                            QVector3D(-1.0f,  1.0f, -1.0f),
+                            QVector3D(-1.0f, -1.0f,  1.0f),
+                            QVector3D(-1.0f, -1.0f, -1.0f),
                             this));
     //right
     m_triangles->append(new Triangle(
-                            QVector3D( value,  value, -value),
-                            QVector3D( value, -value, -value),
-                            QVector3D( value, -value,  value),
+                            QVector3D( 1.0f,  1.0f, -1.0f),
+                            QVector3D( 1.0f, -1.0f, -1.0f),
+                            QVector3D( 1.0f, -1.0f,  1.0f),
                             this));
     m_triangles->append(new Triangle(
-                            QVector3D( value,  value, -value),
-                            QVector3D( value, -value,  value),
-                            QVector3D( value,  value,  value),
+                            QVector3D( 1.0f,  1.0f, -1.0f),
+                            QVector3D( 1.0f, -1.0f,  1.0f),
+                            QVector3D( 1.0f,  1.0f,  1.0f),
                             this));
     //top
     m_triangles->append(new Triangle(
-                            QVector3D(-value,  value, -value),
-                            QVector3D( value,  value, -value),
-                            QVector3D( value,  value,  value),
+                            QVector3D(-1.0f,  1.0f, -1.0f),
+                            QVector3D( 1.0f,  1.0f, -1.0f),
+                            QVector3D( 1.0f,  1.0f,  1.0f),
                             this));
     m_triangles->append(new Triangle(
-                            QVector3D( value,  value,  value),
-                            QVector3D(-value,  value,  value),
-                            QVector3D(-value,  value, -value),
+                            QVector3D( 1.0f,  1.0f,  1.0f),
+                            QVector3D(-1.0f,  1.0f,  1.0f),
+                            QVector3D(-1.0f,  1.0f, -1.0f),
                             this));
     //bottom
     m_triangles->append(new Triangle(
-                            QVector3D(-value, -value,  value),
-                            QVector3D( value, -value,  value),
-                            QVector3D( value, -value, -value),
+                            QVector3D(-1.0f, -1.0f,  1.0f),
+                            QVector3D( 1.0f, -1.0f,  1.0f),
+                            QVector3D( 1.0f, -1.0f, -1.0f),
                             this));
     m_triangles->append(new Triangle(
-                            QVector3D(-value, -value,  value),
-                            QVector3D( value, -value, -value),
-                            QVector3D(-value, -value, -value),
+                            QVector3D(-1.0f, -1.0f,  1.0f),
+                            QVector3D( 1.0f, -1.0f, -1.0f),
+                            QVector3D(-1.0f, -1.0f, -1.0f),
                             this));
 }
 
