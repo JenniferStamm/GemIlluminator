aa3cb156f344b170a986544a83c888c9802a6e1a
Pascal Lange <pascal.lange@student.hpi.uni-potsdam.de>
2015-01-27 18:44:14 +0100

    Correct triangle orientation of CubeGem

--- gemIlluminator/cubegem.cpp
+++ gemIlluminator/cubegem.cpp
@@ -13,86 +13,86 @@ CubeGem::CubeGem(QObject *parent) :
     m_radius = sqrt(3.f);
 
     QVector<QVector3D> vertices;
-    vertices.append(QVector3D(-1.0f, -1.0f, -1.0f)); // front bottom left
-    vertices.append(QVector3D( 1.0f, -1.0f, -1.0f)); // front bottom right
-    vertices.append(QVector3D(-1.0f,  1.0f, -1.0f)); // front top left
-    vertices.append(QVector3D( 1.0f,  1.0f, -1.0f)); // front top right
-    vertices.append(QVector3D(-1.0f, -1.0f,  1.0f)); // back bottom left
-    vertices.append(QVector3D( 1.0f, -1.0f,  1.0f)); // back bottom right
-    vertices.append(QVector3D(-1.0f,  1.0f,  1.0f)); // back top left
-    vertices.append(QVector3D( 1.0f,  1.0f,  1.0f)); // back top right
+    vertices.append(QVector3D(-1.0f, -1.0f, -1.0f)); // back bottom left
+    vertices.append(QVector3D( 1.0f, -1.0f, -1.0f)); // back bottom right
+    vertices.append(QVector3D(-1.0f,  1.0f, -1.0f)); // back top left
+    vertices.append(QVector3D( 1.0f,  1.0f, -1.0f)); // back top right
+    vertices.append(QVector3D(-1.0f, -1.0f,  1.0f)); // front bottom left
+    vertices.append(QVector3D( 1.0f, -1.0f,  1.0f)); // front bottom right
+    vertices.append(QVector3D(-1.0f,  1.0f,  1.0f)); // front top left
+    vertices.append(QVector3D( 1.0f,  1.0f,  1.0f)); // front top right
 
-    // front-face top-left triangle
-    m_triangles->append(new Triangle(
-                vertices.at(0),
-                vertices.at(3),
-                vertices.at(2),
-                this));
-    // front-face bottom-right triangle
-    m_triangles->append(new Triangle(
-                vertices.at(0),
-                vertices.at(1),
-                vertices.at(3),
-                this));
-    // right-face top-left triangle
-    m_triangles->append(new Triangle(
-                vertices.at(1),
-                vertices.at(7),
-                vertices.at(3),
-                this));
-    // right-face bottom-right triangle
-    m_triangles->append(new Triangle(
-                vertices.at(1),
-                vertices.at(5),
-                vertices.at(7),
-                this));
     // back-face top-left triangle
     m_triangles->append(new Triangle(
-                vertices.at(5),
-                vertices.at(6),
-                vertices.at(7),
+                vertices.at(0),
+                vertices.at(2),
+                vertices.at(3),
                 this));
     // back-face bottom-right triangle
     m_triangles->append(new Triangle(
-                vertices.at(5),
-                vertices.at(4),
-                vertices.at(6),
-                this));
-    // left-face top-left triangle
-    m_triangles->append(new Triangle(
-                vertices.at(4),
-                vertices.at(2),
-                vertices.at(6),
-                this));
-    // left-face bottom-right triangle
-    m_triangles->append(new Triangle(
-                vertices.at(4),
                 vertices.at(0),
-                vertices.at(2),
+                vertices.at(3),
+                vertices.at(1),
                 this));
-    // top-face top-left triangle
+    // right-face top-right triangle
     m_triangles->append(new Triangle(
-                vertices.at(2),
-                vertices.at(7),
-                vertices.at(6),
-                this));
-    // top-face bottom-right triangle
-    m_triangles->append(new Triangle(
-                vertices.at(2),
+                vertices.at(1),
                 vertices.at(3),
                 vertices.at(7),
+                this));
+    // right-face bottom-left triangle
+    m_triangles->append(new Triangle(
+                vertices.at(1),
+                vertices.at(7),
+                vertices.at(5),
+                this));
+    // front-face top-left triangle
+    m_triangles->append(new Triangle(
+                vertices.at(5),
+                vertices.at(7),
+                vertices.at(6),
+                this));
+    // front-face bottom-right triangle
+    m_triangles->append(new Triangle(
+                vertices.at(5),
+                vertices.at(6),
+                vertices.at(4),
+                this));
+    // left-face top-right triangle
+    m_triangles->append(new Triangle(
+                vertices.at(4),
+                vertices.at(6),
+                vertices.at(2),
+                this));
+    // left-face bottom-left triangle
+    m_triangles->append(new Triangle(
+                vertices.at(4),
+                vertices.at(2),
+                vertices.at(0),
+                this));
+    // top-face bottom-left triangle
+    m_triangles->append(new Triangle(
+                vertices.at(2),
+                vertices.at(6),
+                vertices.at(7),
+                this));
+    // top-face top-right triangle
+    m_triangles->append(new Triangle(
+                vertices.at(2),
+                vertices.at(7),
+                vertices.at(3),
                 this));
     // bottom-face top-left triangle
     m_triangles->append(new Triangle(
                 vertices.at(4),
-                vertices.at(1),
                 vertices.at(0),
+                vertices.at(1),
                 this));
     // bottom-face bottom-right triangle
     m_triangles->append(new Triangle(
                 vertices.at(4),
-                vertices.at(5),
                 vertices.at(1),
+                vertices.at(5),
                 this));
 }
 
