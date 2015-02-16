311ed030e89c37934351819f323813fc54570fec
Sebastian Koall <sebastian.koall@student.hpi.uni-potsdam.de>
2015-02-16 15:00:41 +0100

    Remove envMapPrefix and add envMap reload to match the new config

--- gemIlluminator/painterqml.h
+++ gemIlluminator/painterqml.h
@@ -13,15 +13,12 @@ class PainterQML : public QQuickItem
 {
     Q_OBJECT
     Q_PROPERTY(bool active READ isActive WRITE setActive NOTIFY activeChanged)
-    Q_PROPERTY(QString envMapPrefix READ envMapPrefix WRITE setEnvMapPrefix NOTIFY envMapPrefixChanged)
     Q_PROPERTY(Scene* scene READ scene WRITE setScene NOTIFY sceneChanged)
 
 public:
     explicit PainterQML(QQuickItem *parent = 0);
     virtual ~PainterQML();
 
-    QString envMapPrefix() const;
-    void setEnvMapPrefix(const QString &envMapPrefix);
     bool event(QEvent *ev) override;
 
     bool isActive() const;
@@ -29,13 +26,14 @@ public:
 
     QEvent::Type paintingDoneEventType();
 
+    Q_INVOKABLE void reloadEnvMap();
+
     Scene *scene() const;
     void setScene(Scene *scene);
 
 signals:
     void activeChanged();
     void sceneChanged();
-    void envMapPrefixChanged();
 
 public slots:
     void synchronize();
@@ -44,8 +42,6 @@ public slots:
 protected:
     bool m_active;
     bool m_isUpdatePending;
-    QString m_envMapPrefix;
-    bool m_isEnvMapInvalidated;
     Painter *m_painter;
     int m_paintingDoneEventType;
     Scene *m_scene;
