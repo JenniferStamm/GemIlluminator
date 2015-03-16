8366e65cdf9f477ea018487d8082b1a70d7e45ac
Pascal Lange <pascal.lange@student.hpi.uni-potsdam.de>
2015-03-16 17:07:54 +0100

    Document ScreenAlignedQuad

--- gemIlluminator/screenalignedquad.h
+++ gemIlluminator/screenalignedquad.h
@@ -4,15 +4,29 @@
 
 class QOpenGLFunctions;
 
+/**
+ * @brief The ScreenAlignedQuad class encapsulates the drawing of a screen aligned quad.
+ */
 class ScreenAlignedQuad
 {
 public:
+    /**
+     * @brief Creates a new ScreenAlignedQuad
+     */
     ScreenAlignedQuad();
     ~ScreenAlignedQuad();
 
+    /**
+     * @brief Draws the ScreenAlignedQuad with bound shader program.
+     * @param gl QOpenGLFunctions thar are used to do gl-calls
+     */
     void draw(QOpenGLFunctions &gl);
 
 protected:
+    /**
+     * @brief Writes vertices into given buffer.
+     * @param vertices The buffer into which the vertices are written
+     */
     static void strip(QOpenGLBuffer &vertices);
 
 private:
